b597923584aea8d973a07696f52d74924e9b5fd9
Maria Clara <161085828+Mariacpdb@users.noreply.github.com>
2024-09-28 13:17:30 -0300

    Update canal.py

--- canal.py
+++ canal.py
@@ -8,7 +8,7 @@ import paho.mqtt.client as mqtt
 API_KEY = "FELVHXOQDN8N7DSE"
 broker = 'localhost'
 port = 1883
-topic = 'sensores/energiasolar'  # Corrigido para ser consistente
+topic = 'sensores/energiasolar'  
 
 def enviar_dados_thingspeak(dados):
     url = f"https://api.thingspeak.com/update?api_key={API_KEY}&field1={dados['irradiancia']}&field2={dados['temperatura']}&field3={dados['corrente']}&field4={dados['voltagem']}&field5={dados['energia_gerada']}"
