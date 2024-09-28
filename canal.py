11ae81aaf4588389722710b534e97a1c98320c9b
Mariacpdb <161085828+Mariacpdb@users.noreply.github.com>
2024-09-28 11:25:26 -0300

    atualizar2

--- canal.py
+++ canal.py
@@ -6,7 +6,7 @@ import time
 API_KEY = "M1B95IS41EVWUCMY"
 
 def enviar_dados_thingspeak(dados):
-    url = f"https://api.thingspeak.com/update?api_key={API_KEY}&field1={temperature}"
+    url = f"https://api.thingspeak.com/update?api_key={API_KEY}&field1={dados}"
     response = requests.get(url)
     print(f"Enviando dados: {dados} para ThingSpeak, Resposta: {response.status_code}")
 
